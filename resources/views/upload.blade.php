49879afc13d6bacb17da51c993934aeaed9460cc
Walid <walidudem@gmail.com>
2023-12-01 20:56:16 -0500

    add upload image form

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -11,5 +11,12 @@
     <h1  class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">Upload images</h1>
 
     <img class="h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800 h-auto max-w-lg mx-auto" src="{{ URL::to('/') }}/images/store.jpg" alt="image description">
+
+    <form class="max-w-md mx-auto">
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+        
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
 </body>
 </html>
