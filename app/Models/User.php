b83afa875de074b29e91d0024ededcace163940b
Walid <walidudem@gmail.com>
2024-03-25 00:03:02 -0400

    Revert "Install Roles and Permissions in laravel and add them to user Model"
    This reverts commit fb0d5e7574363868de8f4a186066ed5280772519.

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,14 +43,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class);
-    }
-
-    public function permissions()
-    {
-        return $this->belongsToMany(Permission::class);
-    }
 }
